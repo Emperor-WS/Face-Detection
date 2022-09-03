0900dd0256c523fcf07c11ab52d614c9397b9425
Waleed Salah Eldin <76920103+Emperor-WS@users.noreply.github.com>
2022-09-03 18:56:59 +0200

    Add files via upload

---
+++ FaceDetectionModule.py
@@ -0,0 +1,78 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class faceDetector:
+    def __init__(self, minDetectionCon = 0.5):
+        
+        self.minDetectionCon = minDetectionCon
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpFaceDetection = mp.solutions.face_detection
+        self.FaceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
+    
+    
+    def findFaces(self, img, draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.FaceDetection.process(imgRGB)
+        # print(self.results)
+        bboxs = []
+        if  self.results.detections:     
+            for id, detection in enumerate( self.results.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                h, w, c = img.shape        
+                bbox = int(bboxC.xmin * w) , int(bboxC.ymin * h), \
+                       int(bboxC.width * w) , int(bboxC.height * h) 
+                bboxs.append([id, bbox, detection.score])
+                if draw:
+                    img = self.fancyDraw(img, bbox)
+                    cv2.putText(img, str(int(detection.score[0]*100)) + "%",
+                            (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_COMPLEX, 1, (255 , 0 , 255), 2)
+        return img , bboxs
+    
+    def fancyDraw(self, img, bbox, l=30, t=5, rt=1):
+        x, y, w, h = bbox
+        x1, y1 = x + w, y + h
+        
+        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        #top left x,y
+        cv2.line(img, (x, y), (x + l, y), (255, 0, 255), t)
+        cv2.line(img, (x, y), (x , y + l), (255, 0, 255), t)
+        #top right x1,y
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1 , y + l), (255, 0, 255), t)
+        #bottom left x,y1
+        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        cv2.line(img, (x, y1), (x , y1 - l), (255, 0, 255), t)
+        #bottm right x1,y1
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1 , y1 - l), (255, 0, 255), t)         
+        return img
+        
+
+def main():
+    #opening the webcam
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    cTime = 0
+    detector = faceDetector()
+    while True:
+        success,img = cap.read()
+        img, bboxs = detector.findFaces(img)
+        #calculating our fps
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX, 3, (0 , 255 , 0), 3)
+        cv2.imshow("Image", img)
+        #closing the window by using q
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+ 
+
+if __name__ == "__main__":
+    main()
